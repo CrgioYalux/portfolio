3cbcfd8931426fdb51c3a66153b2c4c342b9bc86
Sergio Yalux <randaledomman1@gmail.com>
2023-12-29 02:39:10 -0300

    chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App: React.FC = () => {
+  return (
+    <div className='min-h-screen bg-indigo-900 dark:bg-indigo-100 text-indigo-500'>
+      Hello there
+    </div>
+  );
+};
+
+export default App;
