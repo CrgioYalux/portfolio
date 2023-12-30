defc158d6b561573a7a2653b70397bf52133ba11
Sergio Yalux <randaledomman1@gmail.com>
2023-12-30 02:50:32 -0300

    feat: create Home (/) page & header and about sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import UIUXContextProvider from './providers/UIUX';
+
+import Home from './pages/Home';
+
 const App: React.FC = () => {
-  return (
-    <div className='min-h-screen bg-indigo-900 dark:bg-indigo-100 text-indigo-500'>
-      Hello there
-    </div>
-  );
+    return (
+        <UIUXContextProvider>
+            <Home />
+        </UIUXContextProvider>
+   );
 };
 
 export default App;
